44c0dd1e753804e6baaf81ae0bdc7f611e33c4ea
maruška <mariia.gavrylenko@student.gyarab.cz>
2024-06-12 23:42:28 +0200

    views update

--- filmy/views.py
+++ filmy/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
+from django.db.models import Q
+
 from filmy.models import Movie
 # Create your views here.
 
 def movies(request):
+    movies = Movie.objects.all().order_by('name')
+    search = request.GET.get('search')
+    if search:
+        movies = movies.filter(Q(name__icontains=search)|Q(description__icontains=search)) 
     context = {
-        'movies': Movie.objects.all().order_by('name')
+        'movies': movies,
+        "search": search,
     }
     return render (request, 'filmy/movies.html', context)
 
