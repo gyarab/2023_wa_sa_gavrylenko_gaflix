52e986f07eebd5e5523360e01a45e064a4c713d6
maruška <93185278+maruska7@users.noreply.github.com>
2024-06-14 09:58:09 +0200

    update

--- gaflix/settings.py
+++ gaflix/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-l@f+s&vh3k#)_t4wi!pllkl*)35rhticnagiy6owp=0@*h1)+1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
